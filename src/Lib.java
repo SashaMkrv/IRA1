02588e35b05dde6fa11f34c829877561ce155727
sasha <no@no.co>
2018-02-15 15:50:57 -0500

    add TestVals again

--- src/Lib.java
+++ src/Lib.java
@@ -12,7 +12,7 @@ public class Lib {
         //Tokenization + stopword removal
         //Dictionary of tweet ID + TextStringDocument?]
         Map<BigInteger, ir.vsr.TextStringDocument> tweets = new HashMap<BigInteger, ir.vsr.TextStringDocument>();
-        StopWords.changeStopWordsLocation("C:\\Users\\samue\\IdeaProjects\\IRA1\\data\\StopWords.txt");
+        StopWords.changeStopWordsLocation("data\\StopWords.txt");
         ir.vsr.TextStringDocument test = new ir.vsr.TextStringDocument("This is a tweet", true);
         tweets.put(new BigInteger("190000000"), test);
         tweets.get(new BigInteger(("190000000"))).printVector();
