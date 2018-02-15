bb6b7e7ce558c459fb5c3338b869a9ac716f0d63
sasha <no@no.co>
2018-02-15 14:26:39 -0500

    add parser and testvals files, add test map

---
+++ src/TestVals.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestVals {
+
+    static Map<BigInteger, String> docDictionary() {
+        Map<BigInteger, String> map = new HashMap<BigInteger, String>();
+
+        map.put(new BigInteger("1"), "hello");
+        map.put(new BigInteger("2"), "world");
+        map.put(new BigInteger("3"), "hal");
+        map.put(new BigInteger("4"), "daisy");
+
+        System.out.println(map.get(new BigInteger("1")));
+
+        return map;
+    }
+}
